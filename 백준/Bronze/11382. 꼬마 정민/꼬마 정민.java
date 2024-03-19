e9ae7350480219bd50b325b91fd59cecd4f5b91b
yegenie2 <111625018+yegenie2@users.noreply.github.com>
2024-03-19 15:45:01 +0900

    [Bronze V] Title: 꼬마 정민, Time: 208 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		long a = scanner.nextLong();
+		long b = scanner.nextLong();
+		long c = scanner.nextLong();
+		
+		System.out.println(a+b+c);
+	}
+}
