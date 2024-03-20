1b89f88113bb00ab7265192af9bfa8b3acba1706
yegenie2 <111625018+yegenie2@users.noreply.github.com>
2024-03-20 16:39:24 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 420 ms, Memory: 22280 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int n = scanner.nextInt();
+		
+		for( int i=1; i<=n; i++) {
+
+			for(int j = 1; j<=n-i; j++) {
+				System.out.print(" ");
+			}
+			for(int k = 1; k<=i; k++) {
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+	}
+}
