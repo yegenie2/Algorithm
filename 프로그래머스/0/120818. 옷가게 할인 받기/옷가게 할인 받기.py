5c19a69b0a4318f09a29a7300e36b73c8a5ebe13
yegenie2 <111625018+yegenie2@users.noreply.github.com>
2024-03-21 02:06:55 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,11 @@
+def solution(price):
+    answer = 0
+    if price >= 500000:
+        answer=price-(price*0.2)
+    elif price >= 300000:
+        answer=price-(price*0.1)
+    elif price >= 100000:
+        answer=price-(price*0.05)
+    else:
+        answer = price
+    return int(answer)
